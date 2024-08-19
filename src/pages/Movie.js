1280a279dec81eefa5564a5f71b5e338aa78d2ba
Nathanchepkonga <66026997+Nathanchepkonga@users.noreply.github.com>
2024-08-19 09:00:59 +0300

    Update Movie.js

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,16 +1,22 @@
-import { useEffect, useState } from "react";
+import React from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import { movies } from "../data";
 
 function Movie() {
+  const { id } = useParams();
+  const movie = movies.find((movie) => movie.id === parseInt(id));
+
   return (
-    <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Movie info here! */}
-      </main>
-    </>
+    <div>
+      <NavBar />
+      <h1>{movie.title}</h1>
+      <p>{movie.time} minutes</p>
+      {movie.genres.map((genre, index) => (
+        <span key={index}>{genre}</span>
+      ))}
+    </div>
   );
-};
+}
 
 export default Movie;
