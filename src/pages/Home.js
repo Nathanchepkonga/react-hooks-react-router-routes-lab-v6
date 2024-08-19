a97f762ae800ae8c6d3e20e9dae85df3f55f242d
Nathanchepkonga <66026997+Nathanchepkonga@users.noreply.github.com>
2024-08-19 09:22:30 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,26 @@
-import React from "react";
-import NavBar from "../components/NavBar";
+import React, { useState, useEffect } from "react";
 import MovieCard from "../components/MovieCard";
-import { movies } from "../data";
+import NavBar from "../components/NavBar";
 
 function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/movies")
+      .then((response) => response.json())
+      .then((data) => setMovies(data))
+      .catch((error) => console.error("Error fetching movies:", error));
+  }, []);
+
   return (
     <div>
       <NavBar />
       <h1>Home Page</h1>
-      {movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
-      ))}
+      <div>
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
     </div>
   );
 }
