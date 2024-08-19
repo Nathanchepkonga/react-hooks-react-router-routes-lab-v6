37eae4c2437af0ab6b94e30640ee750a55a47d75
Nathanchepkonga <66026997+Nathanchepkonga@users.noreply.github.com>
2024-08-19 09:21:57 +0300

    Update MovieCard.js

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 function MovieCard({ movie }) {
   return (
     <div>
-      <h2>{movie.title}</h2>
-      <Link to={`/movie/${movie.id}`}>More Info</Link>
+      <h3>{movie.title}</h3>
+      <Link to={`/movie/${movie.id}`}>View Details</Link>
     </div>
   );
 }
