bc3ee48a9ee5ac15426e2bbc373560b5be5a7e48
Nathanchepkonga <66026997+Nathanchepkonga@users.noreply.github.com>
2024-08-19 09:11:54 +0300

    Create MovieCard.js

---
+++ src/pages/MovieCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function MovieCard({ movie }) {
+  return (
+    <div>
+      <h2>{movie.title}</h2>
+      <Link to={`/movie/${movie.id}`}>More Info</Link>
+    </div>
+  );
+}
+
+export default MovieCard;
