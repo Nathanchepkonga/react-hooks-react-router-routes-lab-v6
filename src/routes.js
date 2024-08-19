d1da7cd0e14d71dc79e6a8984ddb828f19bdc41d
Nathanchepkonga <66026997+Nathanchepkonga@users.noreply.github.com>
2024-08-19 08:59:04 +0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,34 +1,27 @@
 import Home from "./pages/Home";
 import Directors from "./pages/Directors";
-import Movie from "./pages/Movie";
 import Actors from "./pages/Actors";
+import Movie from "./pages/Movie";
 import ErrorPage from "./pages/ErrorPage";
 
 const routes = [
   {
     path: "/",
     element: <Home />,
-    errorElement: <ErrorPage />,
+    errorElement: <ErrorPage />
   },
   {
     path: "/directors",
-    element: <Directors />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/movie/:id",
-    element: <Movie />,
-    errorElement: <ErrorPage />,
+    element: <Directors />
   },
   {
     path: "/actors",
-    element: <Actors />,
-    errorElement: <ErrorPage />,
+    element: <Actors />
   },
   {
-    path: "*", // Wildcard route for catching unknown routes
-    element: <ErrorPage />,
-  },
+    path: "/movie/:id",
+    element: <Movie />
+  }
 ];
 
 export default routes;
